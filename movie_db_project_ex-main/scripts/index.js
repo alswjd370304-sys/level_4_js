53cf46a93873b4fb69b4d7a0a035966e1ce20647
alswjd370304-sys <alswjd370304@gmail.com>
2026-09-04 11:11:35 +0900

    애니메이션 무비 차트

--- movie_db_project_ex-main/scripts/index.js
+++ movie_db_project_ex-main/scripts/index.js
@@ -42,3 +42,49 @@ for(let i=0; i<9; i++){
     movieLi.innerHTML += `<a><img src="${moiveAnimeDB[i].poster}" alt="${moiveAnimeDB[i].title}"></a>`;
     movieWrap2.appendChild(movieLi);
 }
+
+//---------------------------------------------------------------------------26/9/4
+//무비차트 + swiper
+// swiper-slide 생성해서 swiper-wrapper 붙여넣기
+// swiper 플러그인 자체 함수 연결은 swiper 최종연결
+const chart_swiper = document.querySelector('.chart_swiper'); //붙여넣기 할 대상
+
+const chart_swiper_func = new Swiper(chart_swiper, { //스와이퍼 실행
+    slidesPerView:2,
+    spaceBetween:10,
+});
+
+for(let i=0; i<5; i++){
+    const chart_slide = document.createElement('div'); //div 생성
+    chart_slide.classList.add('swiper-slide'); // div에 클래스로 슬라이드 클래스 연결
+
+    chart_slide.innerHTML = `<p class="num">${movieDB[i].id}위</p>`;
+    chart_slide.innerHTML += `<h3>${movieDB[i].title}</h3>`;
+    chart_slide.innerHTML += `<p class="rating">${movieDB[i].rating}</p>`;
+    chart_slide.innerHTML += `<p class="synopsis">${movieDB[i].synopsis}</p>`;
+    chart_slide.style.backgroundImage = `url(${movieDB[i].poster})`;
+
+    chart_swiper.children[0].appendChild(chart_slide); // 붙여넣기
+}
+
+//애니메이션 무비차트 +swiper
+const anime_swiper = document.querySelector('.anime_chart_swiper'); // 붙여넣기 할 대상
+
+const anime_chart_swiper_func = new Swiper(anime_swiper,{
+    slidesPerView:3,
+    spaceBetween:10,
+}); //스와이퍼 실행
+
+for(let i=0; i<8; i++){
+    const anime_chart_slide = document.createElement('div'); //div 생성
+    anime_chart_slide.classList.add('swiper-slide'); //div에 슬라이드 클래스 붙이기
+
+    anime_chart_slide.innerHTML = `<p class="num">${moiveAnimeDB[i].id}위</p>`;
+    anime_chart_slide.innerHTML += `<h3>${moiveAnimeDB[i].title}</h3>`;
+    anime_chart_slide.innerHTML += `<p>${moiveAnimeDB[i].rating}</p>`;
+    anime_chart_slide.innerHTML += `<p>${moiveAnimeDB[i].director}</p>`;
+    anime_chart_slide.innerHTML += `<p>${moiveAnimeDB[i].synopsis}</p>`;
+    anime_chart_slide.style.backgroundImage = `url(${moiveAnimeDB[i].poster})`;
+
+    anime_swiper.children[0].appendChild(anime_chart_slide); // 붙여넣기
+}
